fe1c462ca712c0b695578dd4e8259dff0e176ecc
Greg Thompson <thompson.glowe@gmail.com>
2016-09-11 16:13:32 -0500

    update bash paths

--- bash_profile.sh
+++ bash_profile.sh
@@ -3,11 +3,10 @@
 # bash_profile
 
 declare -a files=(
-    $HOME/.dotfiles/bash/bash_config # Configuration
-    $HOME/.dotfiles/bash/bash_aliases # Aliases
-    $HOME/.dotfiles/bash/functions/* # Functions
-    $HOME/.dotfiles/bash/bash_prompt # Custom bash prompt
-    $HOME/.dotfiles/bash/bash_paths # Path modifications (must source after `.bash_profile.local` in case of custom `brew` location)
+    $HOME/.bash_config # Configuration
+    $HOME/.bash_aliases # Aliases
+    $HOME/.bash_prompt # Custom bash prompt
+    $HOME/.bash_paths # Path modifications (must source after `.bash_profile.local` in case of custom `brew` location)
 )
 
 # If these files are readable, source them
