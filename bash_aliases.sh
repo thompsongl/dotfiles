151059902a944eb5cffdd63c31cd750c0b15b5f6
Greg Thompson <thompson.glowe@gmail.com>
2018-04-18 11:45:49 -0500

    remove ssh; more git aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -14,6 +14,14 @@ alias -- -="cd -"
 # Git
 alias g="git"
 alias gs="git status"
+alias gco="git checkout"
+alias gl="git pull"
+alias gpom="git pull origin master"
+alias gp="git push"
+alias gd="git diff | mate"
+alias gb="git branch"
+alias gba="git branch -a"
+alias del="git branch -d"
 
 # Reset previous commit, but keep all the associated changes.
 alias goddammit="git reset --soft HEAD^"
